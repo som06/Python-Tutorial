45d7927e81aac264a81b79753837a9c42e3270bd
Lenka <somanath.lenka@kpn.com>
2020-07-14 23:07:39 +0200

    Updated code as per Sadan't review comment

--- weather.py
+++ weather.py
@@ -2,115 +2,106 @@ import os
 import sys
 import csv
 from datetime import datetime
+from pathlib import Path
 
-
-months =[]
+data_folder = Path("C:/Som/Personal Projects/weatherdata/")
 
 def main():
     if str.upper(sys.argv[1]) == 'YEAR':
         yr = sys.argv[2]
-        val1 = yearcalc(yr)
-        reportone(val1)
+        yr_data = year_calculator(yr)
+        yearly_report(yr_data)
     elif str.upper(sys.argv[1]) == 'MONTH':
-        mn = sys.argv[2].split('/')
-        val2 = monthcalc(mn)
-        reporttwo(val2)
+        mo = sys.argv[2].split('/')
+        mo_data = month_calculator(mo)
+        monthly_report(mo_data)
     else:
         print("Please check input parameters")
         exit
 
 
-def yearcalc(y):
-    mx_tmp = 0
-    mx_hum = 0
-    mn_tmp = 100
-    os.chdir(r'C:\Som\Personal Projects\weatherdata')
-    for f in os.listdir():
-        if f.__contains__(y):
-            months.append(f)
-    for month in months:
-        with open (f'C:\\Som\\Personal Projects\\weatherdata\\{month}','r') as data_file:
-            #with open (r'C:\Som\Personal Projects\weatherdata\lahore_weather_1996_Dec.txt','r') as data_file:
-            csv_data = csv.reader(data_file, delimiter=',')
-            csv_data.__next__()
-            csv_data.__next__()
-            for line in csv_data:
-                if line[0].__contains__('<!'):
-                    break
-                else:
-                    if line[1] == '' and line[3] == '' and line[7] == '':
-                        continue
-                    else:
-                        if int(line[1]) >= mx_tmp:
-                            mx_tmp = int(line[1])
-                            mx_dt = line[0]
-                        if int(line[3]) <= mn_tmp:
-                            mn_tmp = int(line[3]) 
-                            mn_dt = line[0]
-                        if int(line[7]) >= mx_hum:
-                            mx_hum = int(line[7]) 
-                            hum_dt = line[0]
-
-
-    def convert_list_to_date(dt):
-        ''.join(dt)
-        dt_obj = datetime.strptime(dt,'%Y-%m-%d')
-        c_dt = dt_obj.strftime("%b %d")
-        return c_dt
-
-    # converted_dt = convert_list_to_string(mx_dt)
-    # dt_obj = datetime.strptime(converted_dt,'%Y-%m-%d')
-    # c_dt = dt_obj.strftime("%b %d")
-    cmx_dt = convert_list_to_date(mx_dt)
-    cmn_dt = convert_list_to_date(mn_dt)
-    chum_dt = convert_list_to_date(hum_dt)
-
-    # print(f"Highest: {mx_tmp}C on {cmx_dt}")
-    # print(f"Lowest: {mn_tmp}C on {cmn_dt}")
-    # print(f"Humidity: {mx_hum}% on {chum_dt}")
-    return [mx_tmp,cmx_dt,mn_tmp,cmn_dt,mx_hum,chum_dt]
-
-def monthcalc(m):
-    highest_tmp = 0
-    lowest_tmp = 0
-    mean_hum = 0
-    cnt = 0
-    str_mn = ''.join(m)
-    dt = datetime.strptime(str_mn, "%Y%m")
-    c_dt = str(dt.strftime("%Y_%b"))
-    os.chdir(r'C:\Som\Personal Projects\weatherdata')
-    for f in os.listdir():
-        #print(f)
-        if f.__contains__(c_dt):
-            with open (f'C:\\Som\\Personal Projects\\weatherdata\\{f}','r') as data_file:
-
+def year_calculator(yr):
+    max_tmp = 0
+    max_hum = 0
+    min_tmp = 100
+    os.chdir(data_folder)
+    for file in os.listdir():
+        if yr in file:
+            with open (f'{data_folder}/{file}','r') as data_file:
                 csv_data = csv.reader(data_file, delimiter=',')
                 next(csv_data)
+                next(csv_data)
+                # csv_data.__next__()
+                # csv_data.__next__()
                 for line in csv_data:
-                    if line[0].__contains__('<!'):
+                    if line[0].startswith("<!"):
                         break
                     else:
                         if line[1] == '' and line[3] == '' and line[7] == '':
                             continue
                         else:
-                            highest_tmp += int(line[1])
-                            lowest_tmp += int(line[3])
-                            mean_hum += int(line[8])
-                            cnt += 1
-            avg_high = highest_tmp/cnt
-            avg_low = lowest_tmp/cnt
-            avg_hum = mean_hum/ cnt
+                            if int(line[1]) >= max_tmp:
+                                max_tmp = int(line[1])
+                                maxtmp_dt = line[0]
+                            if int(line[3]) <= min_tmp:
+                                min_tmp = int(line[3]) 
+                                mintmp_dt = line[0]
+                            if int(line[7]) >= max_hum:
+                                max_hum = int(line[7]) 
+                                maxhum_dt = line[0]
+
+    c_maxtmp_dt = string_to_date(maxtmp_dt)
+    c_mintmp_dt = string_to_date(mintmp_dt)
+    c_maxhum_dt = string_to_date(maxhum_dt)
+
+    return [max_tmp,c_maxtmp_dt,min_tmp,c_mintmp_dt,max_hum,c_maxhum_dt]
+
+def string_to_date(dt):
+    dt_obj = datetime.strptime(dt,'%Y-%m-%d')
+    c_dt = dt_obj.strftime("%b %d")
+    return c_dt
+
+
+def month_calculator(mo):
+    sum_high_tmp = 0
+    sum_low_tmp = 0
+    sum_mean_hum = 0
+    count = 0
+    str_mo = ''.join(mo)
+    dt = datetime.strptime(str_mo, "%Y%m")
+    c_dt = str(dt.strftime("%Y_%b"))
+    os.chdir(data_folder)
+    for file in os.listdir():
+        if c_dt in file:
+            with open (f'{data_folder}/{file}','r') as data_file:
+                csv_data = csv.reader(data_file, delimiter=',')
+                next(csv_data)
+                next(csv_data)
+                for line in csv_data:
+                    if line[0].startswith("<!"):
+                        break
+                    else:
+                        if line[1] == '' and line[3] == '' and line[7] == '':
+                            continue
+                        else:
+                            sum_high_tmp += int(line[1])
+                            sum_low_tmp += int(line[3])
+                            sum_mean_hum += int(line[8])
+                            count += 1
+            avg_high = sum_high_tmp/count
+            avg_low = sum_low_tmp/count
+            avg_hum = sum_mean_hum/ count
             return [avg_high,avg_low,avg_hum]
     
-def reportone(v1):
-    print(f"Highest: {v1[0]}C on {v1[1]}")
-    print(f"Lowest: {v1[2]}C on {v1[3]}")
-    print(f"Humidity: {v1[4]}% on {v1[5]}")
+def yearly_report(data_year):
+    print(f"Highest: {data_year[0]}C on {data_year[1]}")
+    print(f"Lowest: {data_year[2]}C on {data_year[3]}")
+    print(f"Humidity: {data_year[4]}% on {data_year[5]}")
 
-def reporttwo(v2):
-    print(f"Highest Aerage: {v2[0]}C")
-    print(f"Lowest Average: {v2[1]}C")
-    print(f"Average Mean Humidity: {v2[2]}%")
+def monthly_report(data_mo):
+    print(f"Highest Aerage: {data_mo[0]}C")
+    print(f"Lowest Average: {data_mo[1]}C")
+    print(f"Average Mean Humidity: {data_mo[2]}%")
 
 if __name__ == '__main__':
     main()
